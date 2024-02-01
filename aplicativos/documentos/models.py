60b60ae8e0f2417a9915c688a0489ac3b7fa94bc
Rayanne Pacto <rayanne.ribeiro@pactonet.com.br>
2024-02-01 17:43:22 -0300

    relacao de models no banco de dados

--- aplicativos/documentos/models.py
+++ aplicativos/documentos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from aplicativos.funcionarios.models import Funcionario
 # Create your models here.
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    owner = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.descricao
+
--- aplicativos/registro_hora_extra/models.py
+++ aplicativos/registro_hora_extra/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from aplicativos.funcionarios.models import Funcionario
+
+
 # Create your models here.
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
